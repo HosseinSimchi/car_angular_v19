51c619e464f1c1c265c15701b1822c1e523c6a55
h.simchi <simchi.hossein@mapnaec.com>
2025-02-17 15:08:22 +0330

    Edited directories

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { RootComponent } from './root/root/root.component';
+import { rootConfig } from './root/root.config';
+
+bootstrapApplication(RootComponent, rootConfig).catch((err) =>
+  console.error(err)
+);
