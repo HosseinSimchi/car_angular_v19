dd9f43ce77ea234c42d670a628a94c376864997c
h.simchi <simchi.hossein@mapnaec.com>
2025-02-25 14:09:03 +0330

    Login Page

--- src/account/login/login.component.ts
+++ src/account/login/login.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from "../../shared/components/header/header.component";
 
 @Component({
   selector: 'car-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  imports: [HeaderComponent]
 })
 export class LoginComponent implements OnInit {
 
