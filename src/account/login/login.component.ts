62612113f004ef0b6f42ed12ab6f2b1fe6238639
h.simchi <simchi.hossein@mapnaec.com>
2025-03-01 11:29:32 +0330

    Added some UI to the LoginCompoentn

--- src/account/login/login.component.ts
+++ src/account/login/login.component.ts
@@ -1,17 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { HeaderComponent } from "../../shared/components/header/header.component";
 
 @Component({
-  selector: 'car-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
-  imports: [HeaderComponent]
+  selector: "car-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
+  imports: [HeaderComponent],
 })
 export class LoginComponent implements OnInit {
+  // npm install otp-generator (To handle SMS using OTP generator without backend service)
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
